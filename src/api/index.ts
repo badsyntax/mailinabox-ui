fcc9a4b9f4ae717db6f6bfe624df6c741d99b2d4
Richard Willis <willis.rh@gmail.com>
2020-08-02 12:07:23 +0100

    Handle 500's better

--- src/api/index.ts
+++ src/api/index.ts
@@ -21,10 +21,6 @@ const apiConfigParams: ConfigurationParameters = {
 
 const apiConfig = new Configuration(apiConfigParams);
 
-export function updateApiConfig(config: ConfigurationParameters): void {
-  Object.assign(apiConfigParams, config);
-}
-
 export const aliasesApi = new AliasesApi(apiConfig);
 export const userApi = new UserApi(apiConfig);
 export const systemApi = new SystemApi(apiConfig);
@@ -33,6 +29,20 @@ export const dnsApi = new DnsApi(apiConfig);
 export const usersApi = new UsersApi(apiConfig);
 export const webApi = new WebApi(apiConfig);
 
+export function updateApiConfig(config: ConfigurationParameters): void {
+  Object.assign(apiConfigParams, config);
+}
+
+async function getResponseText(response: Response): Promise<string | null> {
+  const { status } = response;
+  // Prevent returning the generic web server response for server errors
+  // as it might contain large chunks of unwanted HTML
+  if (status >= 500 && status <= 599) {
+    return null;
+  }
+  return await response.text();
+}
+
 export async function getRequestFailMessage(
   response: Response | Error
 ): Promise<string> {
@@ -40,9 +50,9 @@ export async function getRequestFailMessage(
     return response.message;
   }
   const { statusText, status } = response;
-  const responseText = await response.text();
+  const responseText = await getResponseText(response);
   return (
-    responseText ??
+    responseText ||
     `Request error: ${statusText}${status ? ' (' + status + ')' : ''}`
   );
 }
